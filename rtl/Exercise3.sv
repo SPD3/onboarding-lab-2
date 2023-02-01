10c81efabf87e65ad75ff96f28b84bf7ba1ee701
SPD3 <sean@ayerdoyles.org>
2023-02-01 17:24:12 -0500

    Exercise 3 complete

--- rtl/Exercise3.sv
+++ rtl/Exercise3.sv
@@ -1,3 +1,5 @@
+
+
 module Exercise3 (
     input clk,
     input nReset,
@@ -6,5 +8,13 @@ module Exercise3 (
     input [15:0] c,
     output [15:0] out
 );
+
+wire[7:0] a_in;
+wire[7:0] b_in;
+
+
+Mystery1 Alpha(a[1:0], b[7:0], c[7:0], a_in);
+Mystery1 Beta(a[3:2], b[15:8], c[15:8], b_in);
+Mystery2 Gamma(clk, nReset, a_in, b_in, out);
 
 endmodule

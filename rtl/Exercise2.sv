e54022e7114fbec5fb1c6b93e44d9416eb3b6050
SPD3 <sean@ayerdoyles.org>
2023-02-01 16:58:23 -0500

    Exercise 2 complete

--- rtl/Exercise2.sv
+++ rtl/Exercise2.sv
@@ -12,5 +12,23 @@ module Exercise2 (
     input [15:0] init,
     output logic [15:0] out
 );
+reg [15:0] current_out;
+reg [15:0] inter_out;
+reg new_bit;
+
+always @(posedge clk)
+  begin
+
+    if (!nReset) begin
+      current_out = init;
+    end else begin
+      new_bit = (current_out[15] + current_out[13] + current_out[12] + current_out[10]) & 1;
+      inter_out = 0;
+      inter_out[0] = new_bit;
+      current_out = (current_out << 1) | (inter_out); 
+    end
+
+  end
+  assign out = current_out;
 
 endmodule
